ab510d6568deecf7023a6f58a5c93bb5f2470a1c
rishabh agnihotri <rishabhragnihotri@gmial.com>
2025-09-15 13:03:20 +0530

    javascript

---
+++ JS/jsdemo.js
@@ -0,0 +1,29 @@
+
+
+var a=5;
+let b=1,c=12;
+const d=13;
+{
+    let e=14;
+    const f=15;
+    var g=16;
+
+console.log(a);
+console.log(b);
+console.log(c);
+console.log(d);
+
+console.log(e);
+
+console.log(f);
+}
+// console.log(e);
+// console.log(f);
+
+                             // types of functions in javascript
+                            // 1. Normal function
+                            // 2.Arrow function
+                            // 3.Anonymous function
+                            // 4.IIFE
+                            // 5.Callback function0---> using callback we can pass the function as a parameter in another function
+                            
---
+++ JS/jsfunction.js
@@ -0,0 +1,39 @@
+// function hello( user='guest' ){
+//     console.log(`Hello $(user)`)
+// }
+// hello('rishabh');
+// hello();
+
+// function sum(a,b=1){
+//     console.log("before return ");
+
+//     return a+b;
+//     console.log('after return');// this is unreachable code 
+
+
+// }
+// console.log(sum(23,34));
+// console.log(sum(1));
+
+// const add=()=>console.log('Hello')
+// add();
+
+
+// const add=(a,b)=>a+b
+// //add(7,5);
+// console.log(add(4,5));
+
+// IIFE example 
+// (function (){
+//     console.log("hello rishabh");
+// })();
+
+
+// function hello(user,callback){
+//     console.log(`Hello $(user)`);
+//     callback();
+
+// }
+// hello('Admin',()=> {
+//     console.log(add(4,5)))}
+// });
