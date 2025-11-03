8e177b8879e8adfd1c2174077765f16dafbb057e
rishabh agnihotri <rishabhragnihotri@gmial.com>
2025-11-03 12:59:22 +0530

    vite framework

---
+++ reactapp/src/App.jsx
@@ -0,0 +1,21 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import Book from './component/Book'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+   <>
+   <div><h1>Book store</h1>
+   
+   </div>
+
+   
+   </>
+  )
+}
+
+export default App
---
+++ reactapp/src/component/Book.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import './Book.css'
+
+const Book=()=>{
+    return(
+        <div>
+            <img src="" width={200} height={200} alt='image'/>
+            <h3>Title:Physics</h3>
+            <h4>Price:348</h4>
+            <button>add to cart</button>
+        </div>
+    )
+}
